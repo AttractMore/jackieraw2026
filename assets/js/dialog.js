cbaec292d3855c408fcf835c5562474b5faa20b8
Roger Knight <roger@rogerknight.info>
2026-07-28 13:01:36 +0200

    dialog polyfill

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -1,6 +1,29 @@
 /**
  * Handle the modal menu
  */
+
+const dialog = document.querySelector("dialog");
+
+if (typeof dialog.showModal === "function") {
+  // Load polyfill script
+  const polyfill = document.createElement("script");
+  polyfill.type = "text/javascript";
+  polyfill.src = "/js/dialog-polyfill.js";
+  document.body.append(polyfill);
+
+  // Register polyfill on dialog element once the script has loaded
+  polyfill.onload = () => {
+    dialogPolyfill.registerDialog(dialog);
+  };
+
+  // Load polyfill CSS styles
+  const polyfillStyles = document.createElement("link");
+
+  polyfillStyles.rel = "stylesheet";
+  polyfillStyles.href = "/css/dialog-polyfill.css";
+  document.head.append(polyfillStyles);
+}
+
 const menu_button = document.querySelector("header .menu-btn");
 const menu_dialog = document.getElementById("modal-menu");
 const menu_dialog_close = document.querySelector("#modal-menu .close-btn");
